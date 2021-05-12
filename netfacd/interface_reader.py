efd6cbdf2ed8c70783941ffb6e3ef9b31b1c8587
JesseBoltuc <jboltuc@gmail.com>
2021-05-12 14:25:48 +0000

    learning about telnet

--- netfacd/interface_reader.py
+++ netfacd/interface_reader.py
@@ -2,15 +2,22 @@
 
 import netifaces
 
-print(netifaces.interfaces())
+def get_ip(interface):
+    return (netifaces.ifaddresses(interface)[netifaces.AF_INET])[0]['addr']
 
-for i in netifaces.interfaces():
-    print('\n********Details of Interface - ' + i + ' ************')
-    try:
-        print((netifaces.ifaddresses(i)[netifaces.AF_LINK])[0]['addr']) # Prints the MAC address
-        print((netifaces.ifaddresses(i)[netifaces.AF_INET])[0]['addr']) # Prints the IP address
-        print('IP: ', end='')  # This print statement will always print IP without an end of line
-        print((netifaces.ifaddresses(i)[netifaces.AF_INET])[0]['addr']) # Prints the IP address
-    except:          # This is a new line
-        print('Could not collect adapter information') # Print an error message
+def get_mac(interface):
+    return (netifaces.ifaddresses(interface)[netifaces.AF_LINK])[0]['addr']
 
+def main():
+    print(netifaces.interfaces())
+    for i in netifaces.interfaces():
+        print('\n********Details of Interface - ' + i + ' ************')
+        try:
+            print('MAC: ', end='')
+            print(get_mac(i))
+            print('IP: ', end='')  # This print statement will always print IP without an end of line
+            print(get_ip(i)) 
+        except:          # This is a new line
+            print('Could not collect adapter information') # Print an error message
+
+if __name__ == "__main__":
